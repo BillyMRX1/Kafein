7a1752f5aee58d42cb79ecdeecf4b33c309f72d8
MrMG23 <naufalrafino23@gmail.com>
2020-09-13 23:30:59 +0700

    commit sixth

--- app/src/main/java/com/putrasamudra/kafein/activity/CartActivity.java
+++ app/src/main/java/com/putrasamudra/kafein/activity/CartActivity.java
@@ -1,10 +1,20 @@
 package com.putrasamudra.kafein.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.putrasamudra.kafein.MainActivity;
 import com.putrasamudra.kafein.R;
+import com.putrasamudra.kafein.adapter.SectionsPagerAdapter;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -12,5 +22,7 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
     }
 }
